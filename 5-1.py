20af0f40ce840850dc429bf98c6a61997a572934
cuberisu <cuberisu@gmail.com>
2021-05-09 02:32:18 +0900

    boolean, if문

---
+++ 5-1.py
@@ -0,0 +1,13 @@
+# 블리안 (boolean) True or False
+
+x = True
+y = False
+print(x), print(y)
+print("\n")
+
+a = 3
+b = 4
+if a > b :
+    print("a가 크다.")
+else:
+    print("False이기 때문에 출력 안 된다.","\n")
